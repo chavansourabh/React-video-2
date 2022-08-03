0881727c3704a0df206c66bc8fa70e5993d274fc
sourabh <sourabhchavan20.sc@gmail.com>
2022-08-03 15:25:00 +0530

    challenge-4-imperative way

--- boxes-challenge/src/App.js
+++ boxes-challenge/src/App.js
@@ -5,15 +5,30 @@ import Box from "./Box";
 function App() {
   const [squares, setSquares] = React.useState(boxes);
 
+  function toggle(id) {
+    console.log(id);
+    setSquares((prevSquares) => {
+      const newSquares = [];
+      for (let i = 0; i < prevSquares.length; i++) {
+        const currentSquare = prevSquares[i];
+        if (currentSquare.id === id) {
+          const updatedSquare = {
+            ...currentSquare,
+            on: !currentSquare.on,
+          };
+          newSquares.push(updatedSquare);
+        } else {
+          newSquares.push(currentSquare);
+        }
+      }
+      return newSquares;
+    });
+  }
+
   const squareElement = squares.map((box) => {
-    function toggle(id) {
-      console.log(id);
-      // setSquares((prevState) => {
-      //   return (prevState = !prevState);
-      // });
-    }
     return <Box on={box.on} key={box.id} id={box.id} handleClick={toggle} />;
   });
+
   return <div>{squareElement}</div>;
 }
 
